5107b00afdbd2c3f70e1f58d3d07b2558b163de9
Bruno Lins Maia <brunolinsm@gmail.com>
2024-02-15 18:48:07 -0300

    Refatorada barra de pesquisa

--- script.js
+++ script.js
@@ -34,10 +34,10 @@ barraDePesquisa.addEventListener('input', filtrarPesquisa)
 
 function filtrarPesquisa() {
     const videos = document.querySelectorAll(".videos__item")
+    const valorFiltro = barraDePesquisa.value.toLowerCase()
 
-    if(barraDePesquisa.value != ""){
-
-    } else {
-        
-    }
+    videos.forEach((video) => {
+        const titulo = video.querySelector(".titulo-video").textContent.toLowerCase()
+        video.style.display = valorFiltro ? titulo.includes(valorFiltro) ? 'block' : 'none' : 'block'
+    })
 }
